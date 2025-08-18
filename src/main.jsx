6358a8d11d335a7c39cbc768c6e2f0c8ba869b5a
Full Stack developer <65180848+henryTeran@users.noreply.github.com>
2025-08-18 13:35:50 +0200

    Corriger l'import du composant App

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/theme/index.css";
-import {App} from "./App";
+import App from "./App";
 import { AuthProvider } from "./contexts/AuthProvider";
 
 ReactDOM.createRoot(document.querySelector("#myApp")).render(
