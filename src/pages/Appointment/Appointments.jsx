336bed9ebbd772e1b9bc67a4d6678664829756aa
Full Stack developer <65180848+henryTeran@users.noreply.github.com>
2025-08-18 12:31:14 +0200

    Corriger l'import manquant de useState

--- src/pages/Appointment/Appointments.jsx
+++ src/pages/Appointment/Appointments.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
